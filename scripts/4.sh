223787ef76db11762e7feda0ebbb0b3e4c078ef3
Eli Mutchnik <eli.mu14@gmail.com>
2025-12-02 13:51:16 +0200

    Update 4.sh

--- scripts/4.sh
+++ scripts/4.sh
@@ -14,14 +14,19 @@ kubectl describe pod task-pv-pod
 
 # Checking the mount
 kubectl exec -it task-pv-pod -- bash
-cd usr/share/nginx/html/
+cd /usr/share/nginx/html/
 echo 1 > index.html
 exit
+kubectl get pv
+kubectl describe pv <ID>
 kubectl debug node/docker-desktop -it --image=busybox -- sh
 kubectl delete -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pod-with-pvc.yaml
 
-
+# Local dir
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pod-empty-dir.yaml
+kubectl exec -it test-pd -- bash
+
+# Config maps
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/config-map.yaml
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/game-config.yaml
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pod-with-config-map.yaml
@@ -29,10 +34,17 @@ kubectl exec -it pod-env-var -- bash
 cat /app/game.properties
 exit
 kubectl describe configmap game-config
+
+# Secrets
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/secret.yaml
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pod-with-secret.yaml
-kubectl delete -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/statefulset.yaml
+
+# StatefullSets
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/statefulset.yaml
+
+# DaemonSets
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/daemonset.yaml
+
+# CronJob
 kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/cronjob.yaml
 kubectl scale sts/postgres --replicas=5
