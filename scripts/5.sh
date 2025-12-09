cb19536361f7444a4b14c46cf75b82502d3ff387
Eli Mutchnik <eli.mu14@gmail.com>
2025-12-09 14:49:24 +0200

    Update 5.sh

--- scripts/5.sh
+++ scripts/5.sh
@@ -2,66 +2,42 @@ kubectl get pods
 
 helm list
 
+# Creating chart
 helm create mychart
 cd mychart
 helm template mychart ./
 
-helm upgrade -i myrelease ./
-# change replicaCount to 3
+# Upgrading and rolling back
 helm upgrade -i myrelease ./
 helm list
+## change replicaCount to 3
+helm upgrade -i myrelease ./
 helm history myrelease
+
+# How helm manage revisions
 kubectl get secrets
+
 helm get manifest myrelease --revision 1
 helm get manifest myrelease --revision 2
 helm rollback myrelease
 
+# Pass external values
 helm get values myrelease
-
-# in a new file called `values-prod.yaml` write replicaCount: 5
+## in a new file called `values-prod.yaml` write replicaCount: 5
 helm upgrade -i myrelease -f values-prod.yaml ./
 helm get values myrelease
 
+# Pushing chart
 helm package ./mychart
-helm registry login registry-1.docker.io -u avielb
-# to create a token:
+## to create a token:
 https://app.docker.com/settings/personal-access-tokens
-helm push mychart-0.1.0.tgz oci://registry-1.docker.io/avielb
+helm registry login registry-1.docker.io -u <username>
+helm push mychart-0.1.0.tgz oci://registry-1.docker.io/<username>
+helm fetch --untar oci://registry-1.docker.io/elimutch/mychart --version 0.1.0
+helm upgrade -i myrelease oci://registry-1.docker.io/elimutch/mychart --version 0.1.0 --set replicaCount=3
 
-
-in Chart.yaml, edit from:
-version: 0.1.0
-to 
-version: 0.2.0
-
-helm upgrade -i myrelease oci://registry-1.docker.io/adamlor/mychart --set replicaCount=1
-
-helm fetch --untar  oci://registry-1.docker.io/adamlor/mychart
-helm history adamapp
-helm rollback adamapp
-helm rollback adamapp 2
-helm get values adamapp --all
-helm get manifest adamapp
-helm plugin install https://github.com/databus23/helm-diff
-
+# Playing with ArifactHub
 https://artifacthub.io/
-
-
-helm uninstall adamapp my-release
-Chart.yaml 
-###########################################
-apiVersion: v2
-appVersion: 1.16.0
-description: A Helm chart for Kubernetes
-name: mychart
-type: application
-version: 0.2.0
-dependencies:
-  - name: redis
-    version: 20.11.4
-    repository: oci://registry-1.docker.io/bitnamicharts
-
-
-
-
 helm upgrade -i redis oci://registry-1.docker.io/bitnamicharts/redis
+
+helm uninstall myrelease redis
