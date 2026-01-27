ddcfe67d66bd79462ac8f5bcaa1188aa875eb950
Eli Mutchnik <eli.mu14@gmail.com>
2026-01-27 03:05:01 +0200

    Update 2.tf

--- terraform/2.tf
+++ terraform/2.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     bucket         = "" # <--- YOUR BUCKET NAME HERE
-    key            = "state/terraform.tfstate"
+    key            = "state/2/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     use_lockfile   = true
