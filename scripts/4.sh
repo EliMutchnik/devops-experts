34e004b73fdd4428af31e3afba0de8ac6e365bc5
Eli Mutchnik <eli.mu14@gmail.com>
2026-05-19 00:56:33 +0300

    Update 4.sh

--- scripts/4.sh
+++ scripts/4.sh
@@ -1,15 +1,15 @@
 kubectl get pods
 
 # Creating a Persistent Volume
-kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pv.yaml
+kubectl apply -f https://raw.githubusercontent.com/EliMutchnik/devops-experts/refs/heads/main/k8s/volumes/pv.yaml
 kubectl describe pv
 
 # Creating a Persistent Volume Claim
-kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pvc.yaml
+kubectl apply -f https://raw.githubusercontent.com/EliMutchnik/devops-experts/refs/heads/main/k8s/volumes/pvc.yaml
 kubectl describe persistentvolumeclaim/task-pv-claim
 
 # Creating a Pod with volume
-kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pod-with-pvc.yaml
+kubectl apply -f https://raw.githubusercontent.com/EliMutchnik/devops-experts/refs/heads/main/k8s/volumes/pod-with-pvc.yaml
 kubectl describe pod task-pv-pod
 
 # Checking the mount
@@ -20,33 +20,33 @@ exit
 kubectl get pv
 kubectl describe pv <ID>
 kubectl debug node/docker-desktop -it --image=busybox -- sh
-kubectl delete -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pod-with-pvc.yaml
+kubectl delete -f https://raw.githubusercontent.com/EliMutchnik/devops-experts/refs/heads/main/k8s/volumes/pod-with-pvc.yaml
 
 # Local dir
-kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pod-empty-dir.yaml
+kubectl apply -f https://raw.githubusercontent.com/EliMutchnik/devops-experts/refs/heads/main/k8s/volumes/pod-empty-dir.yaml
 kubectl exec -it test-pd -- bash
 
 # Config maps
-kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/config-map.yaml
-kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/game-config.yaml
-kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pod-with-config-map.yaml
+kubectl apply -f https://raw.githubusercontent.com/EliMutchnik/devops-experts/refs/heads/main/k8s/volumes/config-map.yaml
+kubectl apply -f https://raw.githubusercontent.com/EliMutchnik/devops-experts/refs/heads/main/k8s/volumes/game-config.yaml
+kubectl apply -f https://raw.githubusercontent.com/EliMutchnik/devops-experts/refs/heads/main/k8s/volumes/pod-with-config-map.yaml
 kubectl exec -it pod-env-var -- bash
 cat /app/game.properties
 exit
 kubectl describe configmap game-config
 
 # Secrets
-kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/secret.yaml
-kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/pod-with-secret.yaml
+kubectl apply -f https://raw.githubusercontent.com/EliMutchnik/devops-experts/refs/heads/main/k8s/volumes/secret.yaml
+kubectl apply -f https://raw.githubusercontent.com/EliMutchnik/devops-experts/refs/heads/main/k8s/volumes/pod-with-secret.yaml
 
 # StatefullSets
-kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/statefulset.yaml
+kubectl apply -f https://raw.githubusercontent.com/EliMutchnik/devops-experts/refs/heads/main/k8s/volumes/statefulset.yaml
 kubectl scale sts/postgres --replicas=5
 
 # DaemonSets
-kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/daemonset.yaml
+kubectl apply -f https://raw.githubusercontent.com/EliMutchnik/devops-experts/refs/heads/main/k8s/volumes/daemonset.yaml
 
 # CronJob
-kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/volumes/cronjob.yaml
+kubectl apply -f https://raw.githubusercontent.com/EliMutchnik/devops-experts/refs/heads/main/k8s/volumes/cronjob.yaml
 
 # Extras: PodDisruptionBudget, PriorityClass, ResourceQuota
