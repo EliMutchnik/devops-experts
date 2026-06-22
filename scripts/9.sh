b78d27a482fc6dcdf36c2f9783d341610884d23f
Eli Mutchnik <eli.mu14@gmail.com>
2026-06-23 02:50:09 +0300

    Update 9.sh

--- scripts/9.sh
+++ scripts/9.sh
@@ -1,25 +1,26 @@
-git clone https://github.com/avielb/advanced-devops.git
-cd advanced-devops/monitoring/k8s-setup
-
 # Create monitoring namespace
 kubectl create namespace monitoring
+kubectl config set-context --current --namespace=monitoring
 
 # Install Prometheus
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add kube-state-metrics https://kubernetes.github.io/kube-state-metrics
-helm upgrade -i prometheus prometheus-community/prometheus --namespace monitoring -f prometheus/values.yml
-kubectl patch ds prometheus-prometheus-node-exporter --type "json" -p '[{"op": "remove", "path" : "/spec/template/spec/containers/0/volumeMounts/2/mountPropagation"}]' -n monitoring
+helm upgrade -i prometheus prometheus-community/prometheus -f https://raw.githubusercontent.com/EliMutchnik/devops-experts/refs/heads/main/monitoring/prometheus/values.yaml
+
+# In case of an error
+# kubectl patch ds prometheus-prometheus-node-exporter --type "json" -p '[{"op": "remove", "path" : "/spec/template/spec/containers/0/volumeMounts/2/mountPropagation"}]' -n monitoring
 
 # Install Grafana
-kubectl apply -f monitoring/grafana/config.yml
 helm repo add grafana https://grafana.github.io/helm-charts
-helm install grafana --namespace monitoring grafana/grafana --set rbac.pspEnabled=false
+helm install grafana grafana/grafana --set rbac.pspEnabled=false
 
 ### Node Exporter
 kubectl -n monitoring port-forward svc/prometheus-prometheus-node-exporter 9100:9100
+# Go to localhost:9100
 
 ### Prometheus Server
 kubectl -n monitoring port-forward svc/prometheus-server 9090:80
+# Go to localhost:9090
 # PromQL examples:
 1. 100 - (avg by (instance) (rate(node_cpu_seconds_total{mode="idle"}[5m])) * 100)
 2. (node_memory_MemTotal_bytes - node_memory_MemAvailable_bytes) / node_memory_MemTotal_bytes * 100
@@ -28,14 +29,17 @@ kubectl -n monitoring port-forward svc/prometheus-server 9090:80
 
 ### Push Gateway
 kubectl -n monitoring port-forward svc/prometheus-prometheus-pushgateway 9091:9091
+# Go to localhost:9091
 echo "some_metric 1125" | curl --data-binary @- http://localhost:9091/metrics/job/some_job/a/b
 
 ### Alert Manager
 kubectl -n monitoring port-forward svc/prometheus-alertmanager 9093:9093
+# Go to localhost:9093
 
 ### Grafana
 kubectl get secret --namespace monitoring grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
 kubectl -n monitoring port-forward svc/grafana 3000:80
+# Go to localhost:3000 -> Login with user admin
 # Setup http://prometheus-server as data source
 
 # kube-state-metrics grafana-dashboard id: 6417
