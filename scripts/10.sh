6469cab4f3bd7aa3203686b4ab4b2fb8f01618be
Eli Mutchnik <eli.mu14@gmail.com>
2026-06-30 01:13:09 +0300

    Refactor script by removing K3S and updating AMI ID
    Removed K3S installation and NGINX deployment configuration. Updated AMI ID for AWS instance.

--- scripts/10.sh
+++ scripts/10.sh
@@ -12,54 +12,6 @@ docker run -d -p 80:80 --name nginx nginx:alpine
 # Getting to the NGINX + edit Security groups with my IP
 docker rm -f nginx
 
-# Installing K3S
-https://k3s.io/
-curl -sfL https://get.k3s.io | sh - 
-
-# Check for Ready node, takes ~30 seconds 
-sudo k3s kubectl get node 
-k3s kubectl get pods -A
-alias kubectl='k3s kubectl'
-
-# Running NGINX (via docker)
-# -------------------
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: nginx-deployment
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app: nginx
-  template:
-    metadata:
-      labels:
-        app: nginx
-    spec:
-      containers:
-      - name: nginx
-        image: nginx:stable-alpine
-        ports:
-        - containerPort: 80
----
-apiVersion: v1
-kind: Service
-metadata:
-  name: nginx-service
-spec:
-  selector:
-    app: nginx
-  ports:
-    - protocol: TCP
-      port: 80
-      targetPort: 80
-  type: LoadBalancer
-# -------------------
-kubectl apply -f nginx.yaml
-kubectl get pods
-
-
 # Calculator and billing
 http://calculator.aws/
 
@@ -96,7 +48,7 @@ locals {
 }
 
 resource "aws_instance" "my_first_instance" {
-  ami           = "ami-07ff62358b87c7116" # Amazon Linux 2 AMI
+  ami           = "ami-06067086cf86c58e6" # Amazon Linux 2 AMI
   instance_type = "t3.micro"
 
   tags = {
@@ -107,3 +59,6 @@ resource "aws_instance" "my_first_instance" {
 output "ip" {
   value = aws_instance.my_first_instance.public_ip
 }
+
+# Add security group with your current ip with port 80
+# Add outputs: instance id, instance state and security group id
